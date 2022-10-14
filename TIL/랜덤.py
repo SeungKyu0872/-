5dbb5035cccb7505d4727079bda209fe12f5248b
SeungKyu0872 <shark087210@gmail.com>
2022-10-14 13:13:40 +0900

    !

--- TIL/랜덤.py
+++ TIL/랜덤.py
@@ -1,14 +1,25 @@
 import random
 
-a = ['웅기', '승규', '무남', '석주', '상현', '대혁', '채은', '윤아', '예은', '형운']
-b = random.sample(a,3)
+a = ['웅기', '승규', '무남', '석주', '상현','채은', '윤아', '예은','성훈','기윤','승민','창영','송섭','이주','현영',
+'현웅','채령','대혁','진문','하림','보영','형운']
+b = random.sample(a,5)
 print(b)
-for i in range(3):
+for i in range(5):
     if b[i] in a:
         a.remove(b[i])
-b = random.sample(a,3)
+b = random.sample(a,5)
 print(b)
-for i in range(3):
+for i in range(5):
+    if b[i] in a:
+        a.remove(b[i])
+b = random.sample(a,4)
+print(b)
+for i in range(4):
+    if b[i] in a:
+        a.remove(b[i])
+b = random.sample(a,4)
+print(b)
+for i in range(4):
     if b[i] in a:
         a.remove(b[i])
 b = random.sample(a,4)
